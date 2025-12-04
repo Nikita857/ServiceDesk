004ce50a7822b6a2161e52ff7060ceb53ed70308
Nikita857 <nikitosbugakov@gmail.com>
2025-12-04 21:28:53 +0300

    feat (validation): add validation constraints in AuthRequest

--- src/main/java/com/bm/wschat/feature/auth/dto/request/AuthRequest.java
+++ src/main/java/com/bm/wschat/feature/auth/dto/request/AuthRequest.java
@@ -1,6 +1,9 @@
 package com.bm.wschat.feature.auth.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record AuthRequest(
-        String username,
-        String password
-) { }
+                @NotBlank(message = "Username is required") String username,
+
+                @NotBlank(message = "Password is required") String password) {
+}
