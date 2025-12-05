0aa0b0eee4d1a11b32b26caca972d4cc3797a096
Nikita857 <nikitosbugakov@gmail.com>
2025-12-05 19:32:27 +0300

    chore: update gitignore

--- src/main/java/com/bm/wschat/feature/ticket/repository/TicketRepositry.java
+++
@@ -1,9 +0,0 @@
-package com.bm.wschat.feature.ticket.repository;
-
-import com.bm.wschat.feature.ticket.model.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TicketRepositry extends JpaRepository<Ticket, String> {
-}
