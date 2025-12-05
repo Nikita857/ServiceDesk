9e32f546a1f23c49268b7bc9e64310e99b95bbbc
Nikita857 <nikitosbugakov@gmail.com>
2025-12-05 20:16:14 +0300

    feat: add new custom exception

---
+++ src/main/java/com/bm/wschat/shared/exception/InvalidTokenException.java
@@ -0,0 +1,7 @@
+package com.bm.wschat.shared.exception;
+
+public class InvalidTokenException extends RuntimeException {
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+}
