f227577ab698411715f29a7f0300bc6b15c5ee6d
Nikita857 <nikitosbugakov@gmail.com>
2025-12-15 20:57:12 +0300

    feat: add ignored propeties to mapper

--- src/main/java/com/bm/wschat/feature/wiki/mapper/WikiArticleMapper.java
+++ src/main/java/com/bm/wschat/feature/wiki/mapper/WikiArticleMapper.java
@@ -8,22 +8,22 @@ import com.bm.wschat.shared.dto.UserShortResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface WikiArticleMapper {
 
     @Mapping(target = "categoryId", source = "category.id")
     @Mapping(target = "categoryName", source = "category.name")
     @Mapping(target = "tags", source = "tagSet")
+    @Mapping(target = "likeCount", ignore = true)
+    @Mapping(target = "likedByCurrentUser", ignore = true)
     WikiArticleResponse toResponse(WikiArticle article);
 
     @Mapping(target = "categoryName", source = "category.name")
     @Mapping(target = "tags", source = "tagSet")
     @Mapping(target = "authorName", expression = "java(article.getCreatedBy().getFio() != null ? article.getCreatedBy().getFio() : article.getCreatedBy().getUsername())")
+    @Mapping(target = "likeCount", ignore = true)
+    @Mapping(target = "likedByCurrentUser", ignore = true)
     WikiArticleListResponse toListResponse(WikiArticle article);
-
-    List<WikiArticleListResponse> toListResponses(List<WikiArticle> articles);
 
     UserShortResponse toUserShortResponse(User user);
 }
