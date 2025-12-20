e30666a5a768b606c1b7ade3a06b12eb9a6e9579
Nikita Bugakov <nikitosbugakov@gmail.com>
2025-12-20 15:39:01 +0300

    refactor: format code and remove logs

--- src/main/java/com/bm/wschat/feature/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/bm/wschat/feature/auth/service/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package com.bm.wschat.feature.auth.service;
 
 import com.bm.wschat.feature.user.model.User;
 import com.bm.wschat.feature.user.repository.UserRepository;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,13 +21,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) {
-        log.debug("Loading user by username: {}", username);
-
-        User user = userRepository.findByUsername(username)
+        return userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
-
-        log.debug("Found user: {}, roles: {}", user.getUsername(), user.getRoles());
-
-        return user;
     }
 }
